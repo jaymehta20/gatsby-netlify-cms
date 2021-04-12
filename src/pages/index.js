ac6f8df625e6c3c966fc2e9daad55cd24a8e1307
Jay Mehta <jake2001007@gmail.com>
2021-04-12 10:39:49 +0530

    Somehow working

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { Link } from "gatsby";
 import Layout from "../components/Layout";
-import SEO from "../components/SEO";
 import { StaticImage } from "gatsby-plugin-image";
 
 const Home = () => {
   return (
     <Layout>
-      <SEO title="Codegrain | Home" />
       <div className="spacer">
         <div className="intro">
           <div className="intro-image">
