423d02cba5f3bb70637016f1e300841ef3f28ff2
Jay Mehta <jake2001007@gmail.com>
2021-04-12 09:42:43 +0530

    Created a SEO component for meta tags

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,32 +6,7 @@ const Layout = ({ children }) => {
   return (
     <>
       <Helmet>
-        <meta charset="utf-8" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1"
-        />
-        <meta name="theme-color" content="#8cc972" />
-        <meta name="robots" content="index,follow" />
-        <meta name="googlebot" content="index,follow" />
-        {/* <title>CodeGrain</title> */}
-        <meta name="format-detection" content="telephone=no" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta
-          content="We strategically design beautiful brands, websites, and digital products that actually work."
-          name="description"
-        />
-        <meta content="We are CodeGrain." property="og:title" />
-        <meta content="CodeGrain" property="og:site_name" />
-        <meta
-          content="We strategically design beautiful brands, websites, and digital products that actually work."
-          property="og:description"
-        />
-        <meta content="./img/logo-square.png" property="og:image" />
-        <meta
-          content="https://codegrain.netlify.com/index.html"
-          property="og:url"
-        />
+        
       </Helmet>
       <header>
         <div className="container">
